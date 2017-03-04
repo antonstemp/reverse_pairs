41ffcde5360dba50de2cfae35e90e96cc0d29220
Anton Pravdin <antonpravdin@gmail.com>
2017-03-04 17:14:13 -0500

    Removed root and removed slicing in create_bst

--- reverse_pairs.py
+++ reverse_pairs.py
@@ -4,8 +4,6 @@ class BSTError(ValueError):
     pass
 
 class BSTNode(object):
-    _root = [None]
-
     def __init__(self, val, parent, size):
         self.val = val
         self.parent = parent
@@ -13,25 +11,11 @@ class BSTNode(object):
         self.left = None
         self.right = None
 
-    @property
-    def root(self):
-        return self._root[0]
-
-    @root.setter
-    def root(self, node):
-        self._root[0] = node
-
-    def delete(self, key):
-        node = self._get(key)
-
-        if node:
-            self._update_size(node)
-
     def _get_right_size(self):
         return self.right.size if self.right else 0
 
     def _get(self, key):
-        node = self.root
+        node = self
         while node and node.val != key:
             node = node.left if node.val > key else node.right
 
@@ -41,15 +25,12 @@ class BSTNode(object):
         return node
 
     def _update_size(self, node):
-        if not node:
-            raise BSTError('update size')
-
         while node:
             node.size -= 1
             node = node.parent
 
     def lower_than(self, n):
-        node = self.root
+        node = self
         count = 0
 
         while node:
@@ -60,6 +41,10 @@ class BSTNode(object):
                 node = node.left
 
         return count
+
+    def delete(self, key):
+        node = self._get(key)
+        self._update_size(node)
 
     def __str__(self):
         return str(self.val)
@@ -84,36 +69,31 @@ class Solution(object):
 
         sorted_nums = sorted(nums)
 
-        lookup = self.create_bst(sorted_nums, None)
-        lookup.root = lookup
+        lookup = self.create_bst(sorted_nums, None, 0, len(sorted_nums))
 
         total = 0
-        for i, n in enumerate(nums[:-1]):
+        for n in nums:
             lookup.delete(n)
             # TODO remove float
             total += lookup.lower_than(float(n) / 2)
 
         return total
 
-    def create_bst(self, nums, parent):
-        size = len(nums)
-
+    def create_bst(self, nums, parent, start, end):
+        size = end - start
         if size == 0:
             return None
 
         if size == 1:
-            return BSTNode(nums[0], parent, 1)
+            return BSTNode(nums[start], parent, 1)
 
         # TODO undo some optimization so that this makes sense
         half = 1 << size.bit_length() - 1
         quarter = half >> 1
-        pivot = quarter - 1 + min(size + 1 - half, quarter)
-
-        left = nums[:pivot]
-        right = nums[pivot + 1:]
+        pivot = start + quarter - 1 + min(size - half + 1, quarter)
 
         node = BSTNode(nums[pivot], parent, size)
-        node.left = self.create_bst(left, node)
-        node.right = self.create_bst(right, node)
+        node.left = self.create_bst(nums, node, start, pivot)
+        node.right = self.create_bst(nums, node, pivot + 1, end)
 
         return node
